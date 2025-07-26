1feefb14506e40ada34eccb99f3bdf9c5809ab3e
paradisoilpapa <paradiso.kumamoto@gmail.com>
2025-07-26 13:33:12 +0900

    Update kv19app.py

--- kv19app.py
+++ kv19app.py
@@ -28,7 +28,7 @@ position_multipliers = {
 }
 
 # --- 基本スコア（脚質ごとの基準値） ---
-base_score = {'逃': 1.533, '両': 1.633, '追': 1.833}
+base_score = {'逃': 1.564, '両': 1.603, '追': 1.834}
 
 # --- 状態保持 ---
 if "selected_wind" not in st.session_state:
